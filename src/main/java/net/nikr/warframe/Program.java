1693e5afdd7b7c152f9512c0718c2acead4b2630
niklaskr <niklaskr@f6ee4b31-964c-6505-1842-63a9e8e2546c>
2014-04-30 10:14:50 +0000

    Making ready to release alpha 2

--- src/main/java/net/nikr/warframe/Program.java
+++ src/main/java/net/nikr/warframe/Program.java
@@ -66,7 +66,7 @@ public class Program {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Program.class);
 
-	public static final String PROGRAM_VERSION = "1.0.0 DEV BUILD 1";
+	public static final String PROGRAM_VERSION = "1.0.0 DEV BUILD 2";
 	public static final String PROGRAM_NAME = "jWarframe";
 
 	private final List<InvasionListener> invasionListeners = new ArrayList<InvasionListener>();
