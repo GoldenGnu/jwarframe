ad1cbb6c5987471f7ba46bbfe26ca1d6b4054f38
niklaskr <niklaskr@f6ee4b31-964c-6505-1842-63a9e8e2546c>
2014-05-28 10:12:09 +0000

    Source: Upload release automatic

--- trunk/src/main/java/net/nikr/warframe/io/update/Updater.java
+++ trunk/src/main/java/net/nikr/warframe/io/update/Updater.java
@@ -3,6 +3,8 @@
  *
  * This file is part of jWarframe.
  *
+ * Original code from jEveAssets (https://code.google.com/p/jeveassets/)
+ *
  * jWarframe is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -21,14 +23,20 @@
 
 package net.nikr.warframe.io.update;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import net.nikr.warframe.Program;
 import net.nikr.warframe.io.shared.FileConstants;
-import net.nikr.warframe.io.shared.ListGetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,27 +44,47 @@ import org.slf4j.LoggerFactory;
 public class Updater {
 	private static final Logger LOG = LoggerFactory.getLogger(Updater.class);
 
+	private static final String UPDATE_URL = "http://warframe.nikr.net/jwarframe/download/update/";
+	private static final String UPDATE =     UPDATE_URL + "jupdate.jar";
+
 	public void update() {
 		LOG.info("Checking online version");
-		VersionGetter getter = new VersionGetter();
-		String version = getter.get();
-		LOG.info("Online: " + version + " Local: " + Program.PROGRAM_VERSION);
-		if (version != null && !version.equals(Program.PROGRAM_VERSION)) {
+		Getter getter = new Getter();
+		final String onlineProgram = getter.get(UPDATE_URL+"update_version.dat");
+		final String localProgram = Program.PROGRAM_VERSION;
+		update("Program", onlineProgram, localProgram, UPDATE_URL);
+	}
+
+	private void update(String title, String online, String local, String link) {
+		LOG.info(title.toUpperCase() + " Online: " + online + " Local: " + local);
+		if (online != null && !online.equals(local)) {
+			boolean download = downloadUpdater();
+			if (!download) {
+				JOptionPane.showMessageDialog(null, "Auto update failed\r\nRestart jEveAssets to try again...", "Auto Update", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
 			int value = JOptionPane.showConfirmDialog(null, 
-					"Update jWarframe now?",
-					"Update Available",
+					title + " update available\r\n"
+					+ "\r\n"
+					+ "Your version: " + local + "\r\n"
+					+ "Latest version: " + online + "\r\n"
+					+ "\r\n"
+					+ "Update " + title.toLowerCase() + " now?\r\n"
+					+ "\r\n"
+					,
+					"Auto Update",
 					JOptionPane.OK_CANCEL_OPTION);
 			if (value == JOptionPane.OK_OPTION) {
-				LOG.info("Updating program");
-				runUpdate();
+				LOG.info("Updating " + title);
+				runUpdate(link);
 			}
 		}
 	}
 
-	private void runUpdate() {
+	private void runUpdate(String link) {
 		ProcessBuilder processBuilder = new ProcessBuilder();
 		processBuilder.directory(getJavaHome());
-		processBuilder.command(getArgsString());
+		processBuilder.command(getArgsString(link));
 		try {
 			processBuilder.start();
 			System.exit(0);
@@ -69,24 +97,66 @@ public class Updater {
 		return new File(System.getProperty("java.home") + File.separator + "bin");
 	}
 
-	private List<String> getArgsString() {
+	private List<String> getArgsString(String link) {
 		List<String> list = new ArrayList<String>();
 		list.add("java");
 		list.add("-jar");
 		list.add(FileConstants.getUpdateJar());
-		list.add("http://warframe.nikr.net/jwarframe/download/update/");
+		list.add(link);
 		list.add(FileConstants.getJar().getAbsolutePath());
 		return list;
 	}
 
-	private static class VersionGetter extends ListGetter {
+	private static boolean downloadUpdater() {
+		DataGetter dataGetter = new DataGetter();
+		Getter getter = new Getter();
+		String checksum = getter.get(UPDATE+".md5");
+		return dataGetter.get(UPDATE, new File(FileConstants.getUpdateJar()), checksum);
+	}
 
-		protected String get() {
-			List<String> list = get("http://warframe.nikr.net/jwarframe/download/update/update_version.dat");
-			if (list.size() == 1) {
-				return list.get(0);
-			} else {
+	private static class Getter {
+
+		protected String get(File file) {
+			try {
+				return get(new FileReader(file));
+			} catch (FileNotFoundException ex) {
 				return null;
+			}
+		}
+
+		protected String get(String link) {
+			try {
+				URL url = new URL(link);
+				return get(new InputStreamReader(url.openStream()));
+			} catch (MalformedURLException e) {
+				return null;
+			} catch (IOException ex) {
+				return null;
+			}
+		}
+		
+		protected String get(Reader reader) {
+			StringBuilder builder = new StringBuilder();
+			try {
+				BufferedReader in = new BufferedReader(reader);
+				
+				String str;
+				while ((str = in.readLine()) != null) {
+					builder.append(str);
+				}
+				return builder.toString();
+			} catch (MalformedURLException e) {
+				return null;
+			} catch (IOException e) {
+				return null;
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException ex) {
+						//I give up...
+					}
+				}
 			}
 		}
 		
