52fb90b8d2e7861870745f0b51eabfb34c2e57ba
niklaskr <niklaskr@f6ee4b31-964c-6505-1842-63a9e8e2546c>
2014-05-28 10:12:09 +0000

    Source: Upload release automatic

---
+++ tools/update/list.php
@@ -0,0 +1,32 @@
+<?php
+
+dirToArray(__DIR__);
+
+function dirToArray($dir, $fix = "", &$first = true) {
+	$handle = opendir($dir);
+	while (($file = readdir($handle)) !== false) {
+		if ($file == '.' || $file == '..') {
+			continue;
+		}
+		if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) {
+			dirToArray($dir . DIRECTORY_SEPARATOR . $file, $fix . $file . "/", $first);
+		} elseif (!str_ends_with($file, ".php") 
+				&& !str_ends_with($file, ".md5")
+				&& !str_ends_with($file, ".dat")
+				&& !str_ends_with($file, "jupdate.jar")) {
+			if ($first) {
+				$first = false;
+			} else {
+				print "\r\n";
+			}
+			print $fix . $file;
+		}
+	}
+	closedir($handle);
+}
+
+function str_ends_with($haystack, $needle) {
+	return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+}
+
+?>
